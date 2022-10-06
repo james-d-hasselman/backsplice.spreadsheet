6443eecfdd92f2206b6dfe9f495a94aa006dd2e7
James D. Hasselman <james.d.hasselman@gmail.com>
2022-10-06 17:15:58 -0400

    Remove unnecessary usings

--- Hasselman.Backsplice.Spreadsheet/ISpreadsheetDocument.cs
+++ Hasselman.Backsplice.Spreadsheet/ISpreadsheetDocument.cs
@@ -1,12 +1,6 @@
 ﻿// SPDX-FileCopyrightText: 2022 James D. Hasselman <james.d.hasselman@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Hasselman.Backsplice.Spreadsheet
 {
     public interface ISpreadsheetDocument
